38d28e130fdff4a8ebdd2b4e4ea398d91a11562e
Andrew Zhuravlev <sir.zhurawell@yandex.ru>
2025-07-26 22:16:32 +0400

    Feature/colour filtered theme (#18)
    * - цвета приложения вынесены в отдельный класс;
    - добавлена заготовка дляпереключения темы;
    
    * - переименованы цвета, убраны ненужные;
    - стили слайдеров, иконок и текста вынесены в темы.
    
    * доделано изменение темы.
    
    * Сделана заготовка для смены тем посредством фильтрации цветов
    
    * Доделана смена темы через фильтрацию цветов
    
    * Поправлен цвет выпадающего меню
    
    * Раскомментирована кнопка смены тем
    
    * Разнёс логику и UI смены тем
    
    * Темы теперь помещены в enum со своими матрицами.
    
    * Исправлена ошибка в матрице темы "Radioactive"
    
    * Переименовано поле
    
    * Легкое изменение кнопки смены темы

--- lib/theme/color_filter_notifier.dart
+++ lib/theme/color_filter_notifier.dart
@@ -1,39 +1,78 @@
 import 'package:flutter/material.dart';
 
-class ColorFilterNotifier extends ChangeNotifier {
-  ColorFilter _matrix = ColorFilter.matrix([
+enum MyTheme {
+  light('Light', [
     1, 0, 0, 0, 0, // R
     0, 1, 0, 0, 0, // G
     0, 0, 1, 0, 0, // B
     0, 0, 0, 1, 0, // A
+  ]),
+  dark('Dark', [
+    -1, 0, 0, 0, 255, // R
+    0, -1, 0, 0, 255, // G
+    0, 0, -1, 0, 255, // B
+    0, 0, 0, 1, 0, // A
+  ]),
+  marina('Marina', [
+    0, 0, 0, 0, 0, // R
+    0, 0, 0, 0, 0, // G
+    0, 0, -1, 0, 255, // B
+    0, 0, 0, 1, 0, // A
+  ]),
+  radioactive('Radioactive', [
+    -0.7, 0, 0.3, 0, 0, // R
+    0.2, 0, 0.2, 0, 0, // G
+    -0.5, 0, 0.7, 0, 0, // B
+    0, 0, 0, 1, 0, // A
+  ]),
+  bloody('Bloody', [
+    1, 0, 0, 0, 0, // R
+    0.2, 0, 0, 0, 0, // G
+    0.2, 0, 0, 0, 0, // B
+    0, 0, 0, 1, 0, // A
+  ]),
+  saint('Saint', [
+    0.7, 0, 0.3, 0, 0, // R
+    0.2, 0, 0.1, 0, 0, // G
+    0.5, 0, 0.7, 0, 0, // B
+    0, 0, 0, 1, 0, // A
+  ]),
+  sporty('Sporty', [
+    0.7, 0, 0.3, 0, 0, // R
+    0.2, 0, 0.1, 0, 0, // G
+    -0.5, 0, 0.7, 0, 0, // B
+    0, 0, 0, 1, 0, // A
   ]);
 
-  ColorFilter get matrix => _matrix;
+  final String _name;
+  final List<double> _matrix;
 
-  void setMatrix({
-    List<double>? redChannel,
-    List<double>? greenChannel,
-    List<double>? blueChannel,
-    List<double>? alphaChannel,
-  }) {
-    assert(
-      (redChannel == null || redChannel.length == 5) &&
-          (greenChannel == null || greenChannel.length == 5) &&
-          (blueChannel == null || blueChannel.length == 5) &&
-          (alphaChannel == null || alphaChannel.length == 5),
-      "ColorFilter channel length is not equal to 5",
-    );
+  @override
+  String toString() => _name;
 
-    ColorFilter newMatrix = _matrix;
-    newMatrix = ColorFilter.matrix([
-      ...redChannel ?? [1, 0, 0, 0, 0], // R
-      ...greenChannel ?? [0, 1, 0, 0, 0], // G
-      ...blueChannel ?? [0, 0, 1, 0, 0], // B
-      ...alphaChannel ?? [0, 0, 0, 1, 0], // A
-    ]);
+  const MyTheme(this._name, this._matrix);
+}
 
-    if (newMatrix != _matrix) {
-      _matrix = newMatrix;
+class ColorFilterNotifier extends ChangeNotifier {
+  late MyTheme _currentTheme;
+  late ColorFilter _filter;
+
+  ColorFilterNotifier() {
+    _currentTheme = MyTheme.light; // TODO: брать из настроек
+    _filter = ColorFilter.matrix(_currentTheme._matrix);
+  }
+
+  ColorFilter get filter => _filter;
+  MyTheme get currentTheme => _currentTheme;
+
+  void setTheme(MyTheme theme) {
+    _currentTheme = theme;
+    _setColorFilter(ColorFilter.matrix(_currentTheme._matrix));
+  }
+
+  void _setColorFilter(ColorFilter filter) {
+    if (filter != _filter) {
+      _filter = filter;
       notifyListeners();
     }
   }
